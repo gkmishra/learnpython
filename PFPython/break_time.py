665a24042177e57e1b1089ce330bbcec40a478a9
Mishra <gmmishra@406c8f15953f.ant.amazon.com>
2015-06-13 14:05:24 -0700

    Modified code to look better by following code style, removed unnecessary project files

--- PFPython/break_time.py
+++
@@ -1,7 +0,0 @@
-import time
-import webbrowser
-i=1
-while (i<=3):
-    time.sleep(10)
-    webbrowser.open("https://www.youtube.com/watch?v=4D2LM-zklac")
-    i = i +1
--- PFPython/movies/media.py
+++ PFPython/movies/media.py
@@ -1,9 +1,12 @@
 import webbrowser
 class Movie():
+    '''Class to provide a blueprint to store movie related details'''
+    # Constructor to initialize basic information
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+    # function to Show the trailer of the selected movie in a web browser
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
